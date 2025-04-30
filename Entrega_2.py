b0be95cb22b8c7e2d40203eeda7a1d5fba9c4183
samuelcoder1 <echeverrysamuel74@gmail.com>
2025-04-30 07:49:34 -0500

    Cambios a entrega 2 y inicio de su documentacion

--- Entrega_2.py
+++ Entrega_2.py
@@ -1,85 +1,110 @@
-'''
-Determinar el estado de aprobación:
-Solicitar al usuario ingresar una calificación numérica (de 0 a 100)
-Evaluar si el estudiante ha aprobado o reprobado basándose en la calificación ingresada
-
-Calcular el promedio:
-Permitir al usuario ingresar una lista de calificaciones (separadas por comas)
-Calcular y mostrar el promedio de las calificaciones en la lista
-
-Contar calificaciones mayores:
-Preguntar al usuario por un valor específico
-Contar cuántas calificaciones en la lista son mayores que este valor
-
-Verificar y contar calificaciones específicas:
-Preguntar al usuario por un valor específico
-Contar cuántas calificaciones en la lista son este valor
-'''
-
-
+# Liberia  importada para el uso de sys.exit
 import sys
 
+# Funcion para el ingreso de notas validas
 def num_valido ():
+    # No se sale de la solicitud hasta tipear una opcion valida
     while True:
         try:
             nota = float(input("Ingresa la nota (0 a 100): "))
+            # Dado que buscamos se ingrese una nota el tipeo valido sera un numero igual o mayor a 0 y menor o igual a 100
             if nota>=0 and nota<=100:
+                # Cumplido el requisito se regreso al valor numerico tipeado y dado este es un return no se necesita del break para salir del while
+                # ya que el return acaba con toda la funcion
                 return nota
-                break
             else:
                 print("Valor invalido")
+        # En caso de intento de ingreso de un caracter no numerico saltara error pero se volvera a realizar la solicitud
         except ValueError:
             print("Valor invalido")
 
+# Dado una nota (notas) correspondiente a una variable numerica y aprobacion (aprobacion) tambien una variable numerica se determina si aprueba o no
 def aprobado (nota, aprobacion):
+    # La forma de comprobacion es mirando si la nota es mayor o igual a la aprobacion regresando asi True o False de acuerdo a esto
     if (nota>=aprobacion):
         return True
     else:
         return False
 
+# Esta funcion le corresponde armar una lista de notas con la entrada de texto del usuario revisando que todo sea cumplido sin problemas
 def cadena_notas ():
+    # Esta funcion no saldra hasta tener una lista de notas valida
     while True:
         try:
             entrada = input("Ingresa tus notas separados por comas (0 al 100): ")
-            
+            # Creamos una lista vacia la cual se ira llenando con las notas entregadas
             valores = []
+            # Como se indico la entreda seran notas separadas por comas "," por eso mismo se realiza un split cada que se encuentre con uno de estos elementos
             for x in entrada.split(","):
+                # Se confirma que las notas ingresadas sean validas (0 al 100) ademas de convertirlas a un nunmero float
                 if 0 <= float(x.strip()) <= 100:
+                    # Si esta se cumple con normalidad las notas seras agregadas a la lista valores
                     valores.append(float(x.strip()))
                 else:
-                    raise ValueError("Nota fuera de rango")
+                    # En caso del tipeo de una nota invalidad se indicara esto y se volvera a realizar la solicitud
+                    raise ValueError("Notas fuera de rango")               
             break
-                    
+        # En caso de que se tipeo un caracter diferente a numeros, comas y espacios en blanco saltara error y se entregara el mensaje            
         except ValueError:
             print("Cadena de valores invalida")
+    # La funcion regresara una lista con las notas que se hayan tipeado
     return valores      
 
+# Esta funcion entregara el promedio de una lista de notas (lista_notas)
 def promedio_notas (lista_notas):
     suma_notas = 0
+    # En principio hace una suma de todas las notas de la lista con la funcion for que recorre esta lista
     for x in lista_notas:
         suma_notas += x
+    # Y luego esta suma de notas la divide por el numero total de elementos de la lista, osea de notas
+    # Regresando asi el promedio
     return suma_notas/len(lista_notas)
 
+# Con esta funcion determino que notas y cuantos son mayores o iguales de una lista de notas (cadena_nota) de acuerdo a una nota especifica (mayor_nota)
 def mayor_nota (cadena_nota, mayor_nota):
+    # En inicio creo una lista vacia donde pondre las notas mayores o igual a
     notas_mayor= []
+    # Recorro los elemento de la lista de notas que me proporcionaron
     for x in cadena_nota:
+        # Evaluo elemento por elemento cual es mayor a la nota especifica
         if x>=mayor_nota:
+            # De serlo lo agrero a mi lista de notas mayores
             notas_mayor.append(x) 
-    print(f"La cantidad de notas mayores a {mayor_nota} es {len(notas_mayor)}")   
-    return notas_mayor
-        
+    # Dado que me lista si cuente con elemento imprimire el mensaje que indica cuantas notas son mayores o igual a la nota especifica
+    # Es bueno aclarar que el if se cumple si la lista no esta vacia, ya que de estar vacia seria False
+    if (notas_mayor):
+        print(f"La cantidad de notas mayores a {mayor_nota} es {len(notas_mayor)}")   
+        # Retorno la cadena con las notas 
+        return notas_mayor
+    else:
+        # Aun cuando sea vacia la retorna para salir de la funcion
+        return notas_mayor
+
+# Con esta funcion determino que notas y cuantos son iguales de una lista de notas (cadena_nota) de acuerdo a una nota especifica (actu_nota)       
 def presente_nota (cadena_nota, actu_nota):
+    # En inicio creo una lista vacia donde pondre las notas igual al a nota especifica
     notas_iguales= []
+    # Recorro los elemento de la lista de notas que me proporcionaron
     for x in cadena_nota:
+        # Evaluo elemento por elemento cual es igual a la nota especifica
         if x==actu_nota:
+            # De serlo lo agrero a mi lista de notas iguales
             notas_iguales.append(x) 
-    print(f"La cantidad de notas iguales a {actu_nota} es {len(notas_iguales)}")   
-    return notas_iguales
+    # Dado que me lista si cuente con elemento imprimire el mensaje que indica cuantas notas igual a la nota especifica
+    if (notas_iguales):
+        print(f"La cantidad de notas iguales a {actu_nota} es {len(notas_iguales)}")   
+    # De estas vacia entonces me indicara que no hay notas iguales a la nota especifica
+    else: 
+        print(f"No hay notas iguales a {actu_nota}")  
     
-            
-            
+#Creo una lista vacia para las notas           
+lista_notas= []            
+
+# Empiezo un while que funcionara para desplegar mi menu 
 while True:
+    # El try se encargara de soportar tipeo de caracteres que no sean los esperados (numericos enteros)
     try:
+        # Imprimo el menu con sus opciones a escoger
         print("-"*30)
         print("Escoje una opcion")
         print("1. Nota aprobatoria")
@@ -97,9 +122,13 @@ while True:
                 nota_aprob = num_valido()
                 aprob = aprobado(nota_actu, nota_aprob)
                 if (aprob):
+                    print("*"*20)
                     print("Aprobaste")
+                    print("*"*20)
                 else:
+                    print("*"*20)
                     print("No aprobaste")
+                    print("*"*20)
                     
             case 2:
                 lista_notas = cadena_notas()
@@ -109,28 +138,32 @@ while True:
                     print("Ingresa la nota mayor")
                     nota_mayor = num_valido()
                     cadena_mayor = mayor_nota(lista_notas, nota_mayor)
-                    print(f"Las notas mayores son: {cadena_mayor}")
+                    if (cadena_mayor):                        
+                        print(f"Las notas mayores son: {cadena_mayor}")
+                    else:
+                        print(f"No hay notas mayores a {nota_mayor}")
                 else:
+                    print("*"*20)
                     print("No existe una lista de notas actualmente, usa la opcion 2")
+                    print("*"*20)
             case 4:
                 if (lista_notas):
                     print("Ingresa la nota a buscar sus repeticiones")
                     nota_igual = num_valido()
-                    cadena_igual = presente_nota(lista_notas, nota_igual)
+                    cadena_igual = presente_nota(lista_notas, nota_igual)                      
                 else:
+                    print("*"*20)
                     print("No existe una lista de notas actualmente, usa la opcion 2")
+                    print("*"*20)
             case 5:
+                print("*"*20)
                 sys.exit("Hasta la proxima")
             case _:
                 print("Numero invalido")
 
     except ValueError:
-        print("Tipeo invalido")
+        print("*"*20)
+        print("[TIPEO INVALIDO]")
+        print("*"*20)
             
-        
-        
-    
-
-'''
-
-'''
+            
